8fabad4041f14340c7c776c707d28dbdef11e1c9
Uze <Uze@yandex.ru>
2015-01-06 22:25:43 +0300

    second

--- sprite-font-builder/src/main/java/org/uze/gfx/sprite/font/builder/SpriteFontBuilderApp.java
+++ sprite-font-builder/src/main/java/org/uze/gfx/sprite/font/builder/SpriteFontBuilderApp.java
@@ -1,21 +1,26 @@
 package org.uze.gfx.sprite.font.builder;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.text.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,9 +33,16 @@ import java.io.IOException;
  */
 public class SpriteFontBuilderApp extends Application {
 
+    public static final double COMBO_BOX_WIDTH = 200.0;
     private final Logger logger = LoggerFactory.getLogger(SpriteFontBuilderApp.class);
     private Stage appStage;
     private final Tab bitmapTab = new Tab("Font Bitmap");
+    private final Label example = new Label();
+    private final ComboBox<FontWeight> fontWeightComboBox = new ComboBox<>();
+    private final ComboBox<FontPosture> fontPostureComboBox = new ComboBox<>();
+    private final ComboBox<Integer> fontSizeComboBox = new ComboBox<>();
+    private final TextField exampleText = new TextField();
+    private final ListView<String> fontListView = new ListView<>();
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -42,20 +54,7 @@ public class SpriteFontBuilderApp extends Application {
         final TabPane tabPane = createTabPane();
         borderPane.setCenter(tabPane);
 
-//        TextFlow textFlow = new TextFlow();
-//        Font font = new Font("Tahoma", 48);
-//
-//        Text text1 = new Text("He said \u0627\u0644\u0633\u0644\u0627\u0645");
-//        text1.setFill(Color.RED);
-//        text1.setFont(font);
-//        Text text2 = new Text(" \u0639\u0644\u064a\u0643\u0645 to me.");
-//        text2.setFill(Color.BLUE);
-//        text2.setFont(font);
-//        textFlow.getChildren().addAll(text1, text2);
-
-        //Group group = new Group(textFlow);
-
-        Scene scene = new Scene(borderPane, 640, 400, Color.WHITE);
+        final Scene scene = new Scene(borderPane, 640, 400, Color.WHITE);
 
         appStage = stage;
 
@@ -69,16 +68,104 @@ public class SpriteFontBuilderApp extends Application {
     private TabPane createTabPane() {
         final TabPane tabPane = new TabPane();
 
-        Tab tab = new Tab();
-        tab.setText("Font Settings");
-        tab.setContent(new Rectangle(200, 200, Color.LIGHTSTEELBLUE));
-        tab.setClosable(false);
-        tabPane.getTabs().add(tab);
-
         bitmapTab.setClosable(false);
-        tabPane.getTabs().add(bitmapTab);
+
+        tabPane.getTabs().addAll(createFontSettingsTab(), createCharacterRangesTab(), bitmapTab);
 
         return tabPane;
+    }
+
+    private Tab createCharacterRangesTab() {
+        final Tab result = new Tab("Character ranges");
+
+        result.setClosable(false);
+
+        VBox vbox = new VBox(4.0);
+        vbox.setPadding(new Insets(4.0));
+        result.setContent(vbox);
+
+        final TextArea charRanges = new TextArea("32-126\n1025\n1040-1105\n9650\n9660");
+        charRanges.setEditable(true);
+        charRanges.setMinHeight(120.0);
+
+        vbox.getChildren().add(charRanges);
+
+        return result;
+    }
+
+    private Tab createFontSettingsTab() {
+        final Tab tab = new Tab();
+
+        tab.setText("Font Settings");
+        tab.setClosable(false);
+
+        final AnchorPane pane = new AnchorPane();
+
+        tab.setContent(pane);
+
+        fontListView.setPrefWidth(250.0);
+        fontListView.setEditable(false);
+        fontListView.setFixedCellSize(Region.USE_COMPUTED_SIZE);
+        fontListView.setCellFactory((listView) -> new FontListCell());
+        fontListView.setItems(FXCollections.observableList(Font.getFamilies()));
+        fontListView.getSelectionModel().selectedItemProperty().addListener((a, b, c) -> onNewFontSelected());
+
+        pane.getChildren().add(fontListView);
+
+        AnchorPane.setTopAnchor(fontListView, 2.0);
+        AnchorPane.setLeftAnchor(fontListView, 2.0);
+        AnchorPane.setBottomAnchor(fontListView, 2.0);
+
+        final VBox vbox = new VBox(4.0);
+        vbox.setPadding(new Insets(4.0));
+        pane.getChildren().add(vbox);
+
+        AnchorPane.setTopAnchor(vbox, 2.0);
+        AnchorPane.setLeftAnchor(vbox, fontListView.getPrefWidth() + 2.0);
+        AnchorPane.setBottomAnchor(vbox, 2.0);
+        AnchorPane.setRightAnchor(vbox, 2.0);
+
+        final Label fontWeightLabel = new Label("Font weight:");
+        fontWeightLabel.setLabelFor(fontWeightComboBox);
+        fontWeightComboBox.setItems(FXCollections.observableArrayList(FontWeight.values()));
+        fontWeightComboBox.getSelectionModel().select(FontWeight.NORMAL);
+        fontWeightComboBox.setPrefWidth(COMBO_BOX_WIDTH);
+        fontWeightComboBox.getSelectionModel().selectedItemProperty().addListener((a, b, c) -> onNewFontSelected());
+
+        final Label fontPostureLabel = new Label("Font posture:");
+        fontPostureLabel.setLabelFor(fontPostureComboBox);
+        fontPostureComboBox.setItems(FXCollections.observableArrayList(FontPosture.values()));
+        fontPostureComboBox.getSelectionModel().select(FontPosture.REGULAR);
+        fontPostureComboBox.setPrefWidth(COMBO_BOX_WIDTH);
+        fontPostureComboBox.getSelectionModel().selectedItemProperty().addListener((a, b, c) -> onNewFontSelected());
+
+        final Label fontSizeLabel = new Label("Font size:");
+        fontSizeLabel.setLabelFor(fontSizeComboBox);
+        fontSizeComboBox.setEditable(true);
+        fontSizeComboBox.setItems(FXCollections.observableArrayList(8, 10, 11, 12, 14, 16, 18, 20, 22, 24, 36, 48));
+        fontSizeComboBox.getSelectionModel().select(Integer.valueOf(10));
+        fontSizeComboBox.setPrefWidth(COMBO_BOX_WIDTH);
+        fontSizeComboBox.getSelectionModel().selectedItemProperty().addListener((a, b, c) -> onNewFontSelected());
+        fontSizeComboBox.setConverter(new IntegerStringConverter());
+
+        exampleText.setPromptText("Leave empty to use font family as example text");
+        exampleText.textProperty().addListener((a, b, c) -> onNewFontSelected());
+
+        example.setAlignment(Pos.TOP_LEFT);
+        example.setPadding(new Insets(4.0));
+        example.setPrefHeight(100.0);
+        example.setMaxHeight(100.0);
+        example.setTextOverrun(OverrunStyle.CLIP);
+        example.setText("Example text");
+
+        vbox.getChildren().addAll(fontWeightLabel, fontWeightComboBox,
+            fontPostureLabel, fontPostureComboBox,
+            fontSizeLabel, fontSizeComboBox,
+            exampleText, example);
+
+        fontListView.getSelectionModel().selectFirst();
+
+        return tab;
     }
 
     private MenuBar createMenu() {
@@ -114,6 +201,57 @@ public class SpriteFontBuilderApp extends Application {
                         logger.error("Save failed!", ex);
                     }
                 }
+            }
+        }
+    }
+
+    private void onNewFontSelected() {
+        if (!StringUtils.isEmpty(exampleText.getText())) {
+            example.setText(exampleText.getText());
+        } else {
+            example.setText(getSelectedFontFamily());
+        }
+        example.setFont(getSelectedFont());
+    }
+
+    private Font getSelectedFont() {
+        return Font.font(
+            getSelectedFontFamily(),
+            getSelectedFontWeight(),
+            getSelectedFontPosture(),
+            getSelectedFontSize()
+        );
+    }
+
+    private double getSelectedFontSize() {
+        return fontSizeComboBox.getSelectionModel().getSelectedItem();
+    }
+
+    private FontPosture getSelectedFontPosture() {
+        return fontPostureComboBox.getSelectionModel().getSelectedItem();
+    }
+
+    private FontWeight getSelectedFontWeight() {
+        return fontWeightComboBox.getSelectionModel().getSelectedItem();
+    }
+
+    private String getSelectedFontFamily() {
+        return fontListView.getSelectionModel().getSelectedItem();
+    }
+
+    private static class FontListCell extends ListCell<String> {
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null) {
+                final Font font = Font.font(item);
+                final Text text = new Text(item);
+
+                text.setFont(font);
+                text.setFontSmoothingType(FontSmoothingType.LCD);
+
+                setGraphic(text);
             }
         }
     }
